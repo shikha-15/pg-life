3a08de3e0fe9e053e4ba084a8868458c90193516
shikha-15 <67531705+shikha-15@users.noreply.github.com>
2022-09-17 12:08:49 +0530

    connected db to railway

--- includes/database_connect.php
+++ includes/database_connect.php
@@ -1,8 +1,11 @@
 <?php
-$conn = mysqli_connect("127.0.0.1", "root", "", "pglife");
+// $conn = mysqli_connect("127.0.0.1", "root", "", "pglife");
+$conn = mysqli_connect("containers-us-west-50.railway.app", "root", "jTJYeQ76W02pB3RPLAWD", "railway", "5493");
 
 if (mysqli_connect_errno()) {
     // Throw error message based on ajax or not
     echo "Failed to connect to MySQL! Please contact the admin.";
     return;
 }
+
+// }Q-lEVC6K#T9[~F1
